82ab03260c28190fcfbbd147a106d36f0fd24cbf
Faysal Ahmmad <faysal4ah@gmail.com>
2018-07-15 10:16:34 +0600

    three four

--- TokenAuthWithPG/Controllers/SimsController.cs
+++ TokenAuthWithPG/Controllers/SimsController.cs
@@ -21,40 +21,7 @@ namespace TokenAuthWithPG.Controllers
             _context = context;
         }
 
-
-        public void Test1()
-        {
-            string Test1 = "Test 1";
-        }
-
-        public void Test()
-        {
-            string test = "Test";
-        }
-
-        public void Test4()
-        {
-            string test = "Test";
-        }
-
-        public void Test5()
-        {
-            string test = "Test";
-        }
-
-        public void Test6()
-        {
-            string test = "Test";
-        }
-
-        public void Test7()
-        {
-            string test = "Test";
-        }
-
-        public void Test8()
-        {
-            string test = "Test";
-        }
+        //comment three
+        //comment four
     }
 }
